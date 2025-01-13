d18c19505633e981841dbaae2b934a883514fc54
agraciadev <andresgracia.3t@gmail.com>
2025-01-13 14:11:23 +0100

    Added boostsrap 5 and base layout
    - Added boostsrap 5
    - Created base layout
    - Created routes books and contact

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/books', [BooksController::class, "index"])->name("books");
+Route::get('/contact', [ContactController::class, "index"])->name("contact");
